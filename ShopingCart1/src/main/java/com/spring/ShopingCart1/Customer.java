fc552bb479e42f9666a421b64f088463f19b356b
Jhon <Jhon@Rishu>
2018-11-18 11:36:05 +0530

    add customer model class

---
+++ ShopingCart1/src/main/java/com/spring/ShopingCart1/Customer.java
@@ -0,0 +1,18 @@
+package com.spring.ShopingCart1;
+
+public class Customer {
+
+	public static void main( String[] args )
+	{
+	    System.out.println( "Hello Prohgra" );
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	}
+
+}
